2448108862498f01f6cd561221ffe9255fa52b66
Arindam Saha <asaha15071998@gmail.com>
2023-02-03 22:55:47 +0530

    All Day 5 Problems

--- src/factor.java
+++ src/factor.java
@@ -5,6 +5,16 @@ public class factor {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter The Number");
         int input = sc.nextInt();
+        for(int i =2; i<input;i++){
+            while(input%i==0){
+                System.out.println(i+"");
+                input=input/i;
+            }
+        }
+        if(input>2){
+            System.out.println(input);
+        }
+        sc.close();
 
     }
 }
